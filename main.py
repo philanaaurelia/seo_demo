b638c8d3eb5e65b36b5be282bfe0398e5eccd6e9
Philana Benton <philanab@gmail.com>
2024-07-02 22:35:50 +0000

    added automation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flask_behind_proxy import FlaskBehindProxy
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+import git
 
 
 # gets name of the .py file so Flask knows it's name
@@ -41,6 +42,16 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/projectname/github-repo-name')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == '__main__':               # this should always be at the end
     app.run(debug=True, host="0.0.0.0")
